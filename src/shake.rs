cb300e38a713443d8d38c977a57d52d5800ce22e
Alex1715 <Alex1715@uw.edu>
2026-02-10 19:02:21 -0800

    Finished Keccakf

--- src/shake.rs
+++ src/shake.rs
@@ -7,27 +7,64 @@ const KECCAKF_RNDC: [u64;24] = [0x0000000000000001, 0x0000000000008082, 0x800000
 0x000000000000800a, 0x800000008000000a, 0x8000000080008081,
 0x8000000000008080, 0x0000000080000001, 0x8000000080008008];
 
-const KECCAKF_ROTC: [i32; 24] = [1,  3,  6,  10, 15, 21, 28, 36, 45, 55, 2,  14,
+const KECCAKF_ROTC: [u32; 24] = [1,  3,  6,  10, 15, 21, 28, 36, 45, 55, 2,  14,
     27, 41, 56, 8,  25, 43, 62, 18, 39, 61, 20, 44];
-const KECCAKF_PILN: [i32;24] =[10, 7,  11, 17, 18, 3, 5,  16, 8,  21, 24, 4,
+const KECCAKF_PILN: [usize;24] =[10, 7,  11, 17, 18, 3, 5,  16, 8,  21, 24, 4,
     15, 23, 19, 13, 12, 2, 20, 14, 22, 9,  6,  1];
 
+const KECCAKF_ROUNDS:usize =24;
+
 fn sha3_keccakf(input: &mut [u8]) {
-    use std::convert::TryInto;
-    let mut i: usize;
     let mut j: usize;
-    let mut r: u32;         // rotation amounts are u32
-    let mut t: u64;          // single temporary lane
-    let mut bc: [u64; 5] = [0; 5]; // temporary column parity
-    let mut st: [u64; 25] = [0; 25]; // 5x5 Keccak state
+    let mut t: u64;
+    let mut bc: [u64; 5] = [0; 5]; 
+    let mut st: [u64; 25] = [0; 25]; // Keccak State
 
     // Load bytes into 64-bit lanes (little-endian)
     for (k, chunk) in input.chunks_exact(8).take(25).enumerate() {
         st[k] = u64::from_le_bytes(chunk.try_into().unwrap());
+    }
+    // Actual Iteration
+    for round in 0..KECCAKF_ROUNDS {
+        // Theta
+        for i in 0..5 {
+            bc[i] = st[i] ^ st[i + 5] ^ st[i + 10] ^ st[i + 15] ^ st[i + 20];
+        }
+        for i in 0..5 {
+            t = bc[(i + 4) % 5] ^ bc[(i + 1) % 5].rotate_left(1);
+            for j in (0..25).step_by(5) {
+                st[j + i] ^= t;
+            }
+        }
+        
+        // Rho Pi
+        t=st[1];
+        for i in  0..24 {
+            j = KECCAKF_PILN[i];
+            bc[0] =st[j];
+            st[j] = t.rotate_left(KECCAKF_ROTC[i]);
+            t=bc[0];
+        }
+        
+        //Chi
+        for j in (0..25).step_by(5) {
+            for i in 0..5 {
+                bc[i]= st[j + i];
+            }
+            for i in 0..5 {
+                st[j + i] ^= (!bc[(i + 1) % 5]) & bc[(i + 2) % 5];
+            }
+        }
+        
+        //Iota
+        st[0] ^= KECCAKF_RNDC[round as usize];
+        
     }
 
     for (k, &lane) in st.iter().enumerate() {
         let bytes = lane.to_le_bytes();
         input[k*8..(k+1)*8].copy_from_slice(&bytes);
     }
+    
+    
 }
